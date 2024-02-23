a9e2b6f46335ed929cec8886928323a70e1e00d3
Pooja <poojadhav313@gmail.com>
2024-02-23 23:36:23 +0530

    modified

--- File rename/file renamer.py
+++ File rename/file renamer.py
@@ -1,13 +1,24 @@
 import os
 
-folder = "." 
+path = "." 
 
-for counter, file in enumerate(os.listdir(folder)):
+files_in_folder = os.listdir(path)
 
-  if file.endswith(".jpg"): 
-    dst = (f"cat {counter}.jpg")
-    src = os.path.join(folder, file)
-    dst = os.path.join(folder, dst)
+index = 0
 
-    os.rename(src, dst)
+try:
+  for file in files_in_folder:
+    if file.endswith(".jpg"): 
 
+        new_filename = f"cat {index}.jpg"
+        
+        old_path = os.path.join(path, file)
+        new_path = os.path.join(path, new_filename)
+
+        os.rename(old_path, new_path)
+        index += 1
+except:
+   print("Can't rename the file with same names")
+   print(Exception)
+else:
+  print("Rename successfull")
